5af2fb404a4cca0245325344206093377acb968b
AppTv <apptv.contato@gmail.com>
2021-06-02 12:13:24 -0300

    StatusBarPage
    StatusBarPage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,24 @@
 import React from "react"
 import { View, Text } from 'react-native'
 
+import { LinearGradient } from 'expo-linear-gradient';
+import StatusBarPage from '../../components/StatusBarPage';
+
+
 export default function Home(){
     return(
-        <View>
+        <LinearGradient
+        colors={['#1ddbb9','#132742']}
+        style={{flex:1, justifyContent: 'center'}}        
+        >
+
+          <StatusBarPage
+          barStyle="ligh-content"
+          backgroundColor="#1ddbb9"
+
+          />        
+
           <Text>Pagina Home</Text>          
-        </View>
+        </LinearGradient>
       )
 }
--- src/pages/MyLinks/index.js
+++ src/pages/MyLinks/index.js
@@ -1,9 +1,16 @@
 import React from "react"
 import { View, Text } from 'react-native'
 
+import StatusBarPage from '../../components/StatusBarPage';
+
 export default function MyLinks(){
     return(
         <View>
+          <StatusBarPage
+          barStyle="ligh-content"
+          backgroundColor="#132742"
+
+          />
           <Text>Pagina Links</Text>          
         </View>
       )
